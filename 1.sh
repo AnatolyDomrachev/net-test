7c528e169b08fd43ffafc906ac78f8c546794083
AnatolyDomrachev <you@example.com>
2019-11-28 19:46:30 +0300

    vimrc

--- 1.sh
+++ 1.sh
@@ -1,1 +1,3 @@
-sed s/"^[+-]"/\\n-/ lab1.txt
+sed s/"^[+-]"/\\n-/ lab1.txt |sed s/^*/------------------------------------------------------\\n*/ > tmp.txt
+vim --cmd "vsplit tmp.txt" begin.txt
+#vim -u vimrc.conf begin.txt
