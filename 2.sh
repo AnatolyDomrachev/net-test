c11a9a7b1430b913e05adfccb797aae4ac6056b1
Your Name <you@example.com>
2019-11-29 12:23:36 +0300

    test results

--- 2.sh
+++ 2.sh
@@ -1,2 +1,5 @@
-sed s/-/+/ result.txt | awk '{print "grep \"" $0 "\" lab1.txt"}' | bash
+echo True ansvers
+sed s/-/+/ result.txt | awk '{print "grep \"" $0 "\" lab1.txt"}' | bash |cat -n # True ansvers
+echo False ansvers
+cat result.txt | awk '{print "grep \"\\" $0 "\" lab1.txt"}' | bash|cat -n   # False ansvers
 
