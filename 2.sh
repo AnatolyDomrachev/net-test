bcb2617026e43647d997cf5052045633f51adcc3
Your Name <you@example.com>
2019-11-30 20:10:39 +0300

    c-file for test answers

--- 2.sh
+++ 2.sh
@@ -1,5 +1,6 @@
 echo True ansvers
-sed s/-/+/ result.txt | awk '{print "grep \"" $0 "\" lab1.txt"}' | bash |cat -n # True ansvers
+grep "^-" begin.py |sed s/-/+/ | awk '{print "grep \"" $0 "\" lab1.txt"}' | bash |cat -n # True ansvers
 echo False ansvers
-cat result.txt | awk '{print "grep \"\\" $0 "\" lab1.txt"}' | bash|cat -n   # False ansvers
+grep "^-" begin.py | awk '{print "grep \"\\" $0 "\" lab1.txt"}' | bash |cat -n # True ansvers
+#cat begin.py | awk '{print "grep \"\\" $0 "\" lab1.txt"}' | bash|cat -n   # False ansvers
 
---
+++ tmp.py
@@ -0,0 +1,2 @@
+sadfdsfsdfsdfsdfsdfsdf
+#sdsdfsdfsdfsdfdsfsdsdfsdf
