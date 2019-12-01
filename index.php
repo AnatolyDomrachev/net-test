52dd73d405dfb30c59e99fb35370fb89f0de662b
AnatolyDomrachev <you@example.com>
2019-12-01 21:03:03 +0300

    php-1

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>input</title>
+  <title>123</title>
  </head>
  <body>
+<?php
+$fin = file("lab1.txt");
+echo "<pre>";
+print_r($fin[2][0]);
 
+?>
 <form name="test" method="post" action="input.php">
+
 <input type=radio name = a[0] value = 'qwe asd zxc'> qwe asd zxc<p>
 <input type=radio name = a[0] value = 'qwe asd zxc0'> qwe asd zxc0<p>
 <input type=radio name = a[0] value = 'qwe asd zxc1'> qwe asd zxc1<p>
+
+<input type=radio name = a[1] value = 'qwe asd zxc'> qwe asd zxc<p>
+<input type=radio name = a[1] value = 'qwe asd zxc0'> qwe asd zxc0<p>
+<input type=radio name = a[1] value = 'qwe asd zxc1'> qwe asd zxc1<p>
+
 <input type=submit>
 
  </body>
---
+++ index0.html
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>input</title>
+ </head>
+ <body>
+
+<form name="test" method="post" action="input.php">
+
+<input type=radio name = a[0] value = 'qwe asd zxc'> qwe asd zxc<p>
+<input type=radio name = a[0] value = 'qwe asd zxc0'> qwe asd zxc0<p>
+<input type=radio name = a[0] value = 'qwe asd zxc1'> qwe asd zxc1<p>
+
+<input type=radio name = a[1] value = 'qwe asd zxc'> qwe asd zxc<p>
+<input type=radio name = a[1] value = 'qwe asd zxc0'> qwe asd zxc0<p>
+<input type=radio name = a[1] value = 'qwe asd zxc1'> qwe asd zxc1<p>
+
+<input type=submit>
+
+ </body>
+</html>
